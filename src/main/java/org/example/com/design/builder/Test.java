d27337491fc8b7c314d599c342d5067c70b614fb
Rashindra Yadav <yadavrashindra5@gmail.com>
2023-08-25 01:47:39 +0530

    design setup

--- src/main/java/org/example/com/design/builder/Test.java
+++
@@ -1,8 +0,0 @@
-package org.example.com.design.builder;
-
-public class Test {
-    public static void main(String[] args) {
-        Phone phone=new PhoneBuilder().setOs("window").setBattery(6).getPhone();
-        System.out.println(phone);
-    }
-}
